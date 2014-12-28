b9fb57c52d16bda8ad7f30827f687ad88af05b49
Ivan Nikolov <nikolovivann@gmail.com>
2014-12-28 19:24:36 +0000

    Made the years used more dynamic and will not have to worry about counting the years every time.

--- scripts/before_load.js
+++ scripts/before_load.js
@@ -2,3 +2,8 @@ function getCurrentYear() {
 	var currDate = new Date();
 	return currDate.getFullYear();
 }
+
+function writeYearsUsed(started) {
+	var currYear = getCurrentYear();
+	document.write(currYear - started);
+}
